feff76ba92210417cb7c41b61f85835be5796318
Kacper Zys <kacperzys13@gmail.com>
2024-03-02 18:33:40 +0100

    implemented fetch API to get content from data.json and display it

--- javascript/destination.js
+++ javascript/destination.js
@@ -1,6 +1,24 @@
-document.querySelectorAll('.main__list-item').addEventListener('click', getData);
+document.querySelectorAll('.main__list-item').forEach(element => {
+    element.addEventListener('click', getData);
+});
 
 function getData(e) {
-    const imagePlace = document.querySelector('.main__image');
+    const image = document.querySelector('.main__image');
+    const name = document.querySelector('.main__text-h1');
+    const description = document.querySelector('.main__text-container');
 
+    fetch('../assets/data.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            return json.destinations[e.target.value];
+        })
+        .then(obj => {
+            console.log(obj);
+            document.querySelector('.main__image-container-img').src = obj.images.webp;
+            document.querySelector('.main__text-title').innerHTML = obj.name;
+            document.querySelector('.main__text-container').innerHTML = obj.description;
+        })
+        .catch(err => console.log(err));
 }
